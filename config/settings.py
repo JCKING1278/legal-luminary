ac0cf4406a1bee1d9a0e5b6929fbabced03f9af2
Scott Weeden <sweeden@ttu.edu>
2026-02-16 15:19:03 -0600

    modified:   config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,2 +1,3 @@
 validate_domains = "data.texas.gov"
 fraudulent_domains = ["www.bing.com", "beltx.mugshots.zone", "texas.arrests.org", "recentlybooked.com", "bellcountytx.com", "texasinmatesearch.org"]
+verified_trusted_domains = ["edtexweblog.com", "solidcounsel.com", "mbaattorneys.com"]
